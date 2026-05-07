0a7fd0e54da6b23bef4c1d8168462894320687e2
Danya Bucenin <danyabucenin@gmail.com>
2026-05-07 16:11:36 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    if(argc < 2)
+    {
+        return 1;
+    }
+    else
+    {
+        std::cout << argv[1] << '\n';
+        return 0;
+        }
+}
